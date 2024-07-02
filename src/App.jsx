dac86da05f7599313017e8c2594cb48f5a0e077e
ahmedgawish10 <ahmdgawish44@gmail.com>
2024-07-03 01:57:34 +0300

    add aniamtion for page home

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,43 @@
-import { useState } from 'react'
+import { useState ,useEffect} from 'react'
 import Header from "../src/components/header/Header"
 import './App.css'
 import MainHeader from './components/header/MainHeader';
 import Hero from './components/hero/Hero';
 import BestProducts from './components/bestProducts/BestProducts';
+// import Loading from './components/loading/Loading';
+import Loading from './components/loding/Loading';
 
 function App() {
   const [count, setCount] = useState(0)
+  const [loading, setLoading] = useState(1);
 
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false); 
+    }, 2000); 
+  }, []);
   return (
-    <>
-    <div className=' App '>
-        <Header />
+<div className="App">
+      {loading ? (
+        <div className="loading-screen">
+       
+          <Loading/>
+        </div>
+      ) : (
+        <>
+           <Header />
         <MainHeader/>
         <Hero/>
         <BestProducts/>
-
+        </>
+      )}
     </div>
-    </>
+
+
+
+
+    
+    
   )
 }
 
